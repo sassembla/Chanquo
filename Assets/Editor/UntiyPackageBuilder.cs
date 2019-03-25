7dba7784be92f6b08ec447d7f9eaaf467e0c462e
sassembla <toru.inoue@kissaki.tv>
2019-03-25 11:34:23 +0900

    added: initial release.

---
+++ Assets/Editor/UntiyPackageBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[InitializeOnLoad]
+public class UnityPackageGenerator
+{
+    [MenuItem("Window/Autoya/Update UnityPackage")]
+    public static void UnityPackage()
+    {
+        var assetPaths = new List<string>();
+
+        var frameworkPath = "Assets/Chanquo";
+        CollectPathRecursive(frameworkPath, assetPaths);
+
+        AssetDatabase.ExportPackage(assetPaths.ToArray(), "Chanquo.unitypackage", ExportPackageOptions.IncludeDependencies);
+    }
+
+    private static void CollectPathRecursive(string path, List<string> collectedPaths)
+    {
+        var filePaths = Directory.GetFiles(path);
+        foreach (var filePath in filePaths)
+        {
+            collectedPaths.Add(filePath);
+        }
+
+        var modulePaths = Directory.GetDirectories(path);
+        foreach (var folderPath in modulePaths)
+        {
+            CollectPathRecursive(folderPath, collectedPaths);
+        }
+    }
+
+    static UnityPackageGenerator()
+    {
+        // create unitypackage if compiled.
+        if (!EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            UnityPackage();
+        }
+    }
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Romantic Versioning](http://blog.legacyteam.info/2015/12/romver-romantic-versioning/).
+
+## [Unreleased]
+
+## [0.8.0] - 2019/03/25
+
+### Changed
+- first release.
+
+### Added
+- initial resources are added.
+
+### Fixed
+- nothing.
+
+### Removed
+- nothing.
+
+
+[Unreleased]: https://github.com/Chanquo/autoya/compare/0.8.0...HEAD
+[0.1.0]: https://github.com/sassembla/Chanquo/compare/0.8.0...0.8.0
